73d0fdf9a01b436e5f5321f71a40d779db463abf
Ben <b.cylin@gmail.com>
2014-05-06 22:28:46 +0800

    Initial Commit

---
+++ Demo/NEUPagingSegmentedControlDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NEUPagingSegmentedControlDemo
+//
+//  Created by Ben on 06/05/2014.
+//  Copyright (c) 2014 bcylin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NEUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NEUAppDelegate class]));
+    }
+}
